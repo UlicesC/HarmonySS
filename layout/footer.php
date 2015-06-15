1f7ccb0e5a3a8664c1f61095db2a6f7bbc2c1b64
UlicesC <ulicescarlosmr@gmail.com>
2015-06-15 11:58:44 -0400

    Todos los archivos y carpetas subidos

---
+++ layout/footer.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2013 <a target="_blank" href="http://shapebootstrap.net/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">ShapeBootstrap</a>. All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="services.php">Servicios</a></li>
+                        <li><a href="#">Faq</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
